40499a5d8d6fc07fc47471660d1e422350f20073
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-10-08 18:34:52 +0300

    my-solution

--- src/schemas/profiles.py
+++ src/schemas/profiles.py
@@ -1,13 +1,83 @@
 from datetime import date
 
-from fastapi import UploadFile, Form, File, HTTPException
-from pydantic import BaseModel, field_validator, HttpUrl
+from fastapi import UploadFile, Form, File, HTTPException, status
+from pydantic import BaseModel, ConfigDict
 
 from validation import (
     validate_name,
     validate_image,
     validate_gender,
-    validate_birth_date
+    validate_birth_date,
 )
 
-# Write your code here
+
+class ProfileCreateRequest:
+    def __init__(
+        self,
+        first_name: str = Form(...),
+        last_name: str = Form(...),
+        gender: str = Form(...),
+        date_of_birth: date = Form(...),
+        info: str = Form(...),
+        avatar: UploadFile = File(...),
+    ):
+        try:
+            validate_name(first_name)
+        except ValueError as e:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=str(e)
+            )
+
+        try:
+            validate_name(last_name)
+        except ValueError as e:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=str(e)
+            )
+
+        try:
+            validate_gender(gender)
+        except ValueError as e:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=str(e)
+            )
+
+        try:
+            validate_birth_date(date_of_birth)
+        except ValueError as e:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=str(e)
+            )
+
+        if not info or info.strip() == "":
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                detail="Info field cannot be empty or contain only spaces.",
+            )
+
+        try:
+            validate_image(avatar)
+        except ValueError as e:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=str(e)
+            )
+
+        self.first_name = first_name.lower()
+        self.last_name = last_name.lower()
+        self.gender = gender
+        self.date_of_birth = date_of_birth
+        self.info = info
+        self.avatar = avatar
+
+
+class ProfileResponseSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    user_id: int
+    first_name: str
+    last_name: str
+    gender: str
+    date_of_birth: date
+    info: str
+    avatar: str
